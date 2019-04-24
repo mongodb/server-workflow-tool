a26e7d8d98d4010bef04bf036334d1295fad05ea
Robert Guo <robertguo@me.com>
2019-04-24 01:24:42 -0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="server_workflow_tool",
-    version="1.0",
+    version="1.0.1",
     description="MongoDB Server Team Workflow Tool",
     long_description=long_description,
     long_description_content_type="text/markdown",
