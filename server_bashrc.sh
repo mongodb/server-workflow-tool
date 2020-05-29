d804af443a1f92f2514738e1abd62b589915635f
Robert Guo <robertguo@me.com>
2020-05-29 13:26:13 -0400

    TIG-2496 setup commands for virtual workstation

---
+++ server_bashrc.sh
@@ -0,0 +1,21 @@
+# Default bashrc for Server development
+
+export NINJA_STATUS='[%f/%t (%p) %es] '
+
+function activate() {
+    source python3-venv/bin/activate
+}
+
+function buildninjaic() {
+    activate
+        ./buildscripts/scons.py  \
+            CCFLAGS='-gsplit-dwarf' \
+            --variables-files=etc/scons/mongodbtoolchain_stable_gcc.vars \
+            MONGO_VERSION=$(git describe --abbrev=0 | tail -c+2) \
+            --ssl \
+            ICECC=icecc CCACHE=ccache --ninja \
+            build.ninja
+    deactivate
+}
+
+alias cr="~/kernel-tools/codereview/upload.py --check-clang-format --check-eslint"
