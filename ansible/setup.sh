3e96b0bdc50289875e5c2f5c5b77d0e1d2fb29fb
jasurbeknurboyev <998946972365>
2022-03-04 19:07:13 +0500

    Added oh-my-zsh integration, role selection now can be skipped, enhanced Ansible installation and playbook execution

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,20 +1,52 @@
 #!/usr/bin/env bash
 
-# install Ansible
+# global variables
 PYTHON=/opt/mongodbtoolchain/v3/bin/python3
+GLOBAL_PYTHON=python3
 
-if [ -f "$PYTHON" ]; then
+# === START: function definitions
+function install_ansible() {
     echo "Installing Ansible."
-    $PYTHON -m venv venv
+    $1 -m venv venv
     source venv/bin/activate
     python -m pip install ansible
-else
-  echo "python3 not found inside mongodbtoolchain. We searched for $PYTHON, but could not find it."
-  exit 1
-fi
+}
+
+function check_python_and_install_ansible() {
+  if [ -f "$PYTHON" ]; then
+      install_ansible $PYTHON;
+  else
+
+    if command -v "$GLOBAL_PYTHON" &> /dev/null; then
+      install_ansible $GLOBAL_PYTHON;
+    else
+      echo "Python not found."
+      echo "We searched for $PYTHON and $GLOBAL_PYTHON, but could not find it."
+      exit 1
+
+    fi
+  fi
+}
+
+function check_ansible_installation {
+    if command -v ansible --version 2>&1 /dev/null; then
+      echo "Ansible successfully installed."
+    else
+      echo "Ansible installation failed."
+      exit 1
+    fi
+}
+
+# === START: flow
+# install ansible
+check_python_and_install_ansible;
+
+# validate ansible installation
+check_ansible_installation;
 
 # run playbook
-ansible-playbook start.yml
+# --become-user and --become are needed to provide ansible with necessary permissions
+ansible-playbook --become-user "$(whoami)" --become start.yml
 # everything is handled by ansible.
 # this script is used to map installing ansible and running the playbook.
 # end
